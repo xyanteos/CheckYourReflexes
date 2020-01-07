c206bf422b72326b72ae5932250b93350f96e352
Paweł Piwkowski <pawel.piwkowski@o2.pl>
2020-01-07 23:38:34 +0100

    Update

--- app/src/main/java/MongoAPI.java
+++ app/src/main/java/MongoAPI.java
@@ -1,6 +1,6 @@
 public class MongoAPI {
 
-    public String getBaseUrl()
+    public String getUrl()
     {
         return "mongodb+srv://check123:check123@cluster0-dq8rc.mongodb.net/test?retryWrites=true&w=majority";
     }
