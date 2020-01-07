e6a90844e0d38ce4092240f4f0b108c08fdad01d
Maciej N <xyanteos@gmail.com>
2020-01-08 00:31:58 +0100

    reversing bugs

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,26 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.mongodb.lang.NonNull;
-import com.mongodb.stitch.android.core.Stitch;
-import com.mongodb.stitch.android.core.StitchAppClient;
-import com.mongodb.stitch.android.core.auth.StitchUser;
-import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
-import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
-import com.mongodb.stitch.core.auth.providers.anonymous.AnonymousCredential;
-import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateOptions;
-import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateResult;
-import org.bson.Document;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
 
@@ -74,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 triesTextView.setText(String.valueOf(tries));
             }
         });
+
         buttonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,8 +76,10 @@ public class MainActivity extends AppCompatActivity {
                 //sendOnServer();
             }
         });
-    }
 
+
+
+    }
 
     private void startTimer() {
         buttonSendOnServer.setVisibility(View.INVISIBLE);
@@ -130,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
         //ponizej jest zawarta logika obliczania punktow zdobydych przez uzytkownika
         if((int)timeLeftInMilis/1000.000<1)
         {
-            score = Math.round(-(((int)timeLeftInMilis/1000.000)-1) * 1e3)  / 1e3;//1e3 jest wykorzystywane do ograniczenia wynikow do 3 miejsc po przecinku.
+            score = Math.round(-(((int)timeLeftInMilis/1000.000)-1) * 1e3)  / 1e3;
         }
         else if((int)timeLeftInMilis/1000.000==1){
             score = 0;
@@ -143,76 +130,5 @@ public class MainActivity extends AppCompatActivity {
         scoreTextView.setText(String.valueOf(score));
 
         buttonSendOnServer.setVisibility(View.VISIBLE);
-
-        buttonSendOnServer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-        //Tu je onclick do wysyłania wyników
-
-            }
-        });
-
-
-
-
-
-
-        final StitchAppClient client =
-                Stitch.initializeDefaultAppClient("checkyourreflex-dcdzf");
-
-        final RemoteMongoClient mongoClient =
-                client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
-
-        final RemoteMongoCollection<Document> coll =
-                mongoClient.getDatabase("<DATABASE>").getCollection("<COLLECTION>");
-
-        client.getAuth().loginWithCredential(new AnonymousCredential()).continueWithTask(
-                new Continuation<StitchUser, Task<RemoteUpdateResult>>() {
-
-                    @Override
-                    public Task<RemoteUpdateResult> then(@NonNull Task<StitchUser> task) throws Exception {
-                        if (!task.isSuccessful()) {
-                            Log.e("STITCH", "Login failed!");
-                            throw task.getException();
-                        }
-
-                        final Document updateDoc = new Document(
-                                "owner_id",
-                                task.getResult().getId()
-                        );
-
-                        updateDoc.put("number", 42);
-                        return coll.updateOne(
-                                null, updateDoc, new RemoteUpdateOptions().upsert(true)
-                        );
-                    }
-                }
-        ).continueWithTask(new Continuation<RemoteUpdateResult, Task<List<Document>>>() {
-            @Override
-            public Task<List<Document>> then(@NonNull Task<RemoteUpdateResult> task) throws Exception {
-                if (!task.isSuccessful()) {
-                    Log.e("STITCH", "Update failed!");
-                    throw task.getException();
-                }
-                List<Document> docs = new ArrayList<>();
-                return coll
-                        .find(new Document("owner_id", client.getAuth().getUser().getId()))
-                        .limit(100)
-                        .into(docs);
-            }
-        }).addOnCompleteListener(new OnCompleteListener<List<Document>>() {
-            @Override
-            public void onComplete(@NonNull Task<List<Document>> task) {
-                if (task.isSuccessful()) {
-                    Log.d("STITCH", "Found docs: " + task.getResult().toString());
-                    return;
-                }
-                Log.e("STITCH", "Error: " + task.getException().toString());
-                task.getException().printStackTrace();
-            }
-        });
-
     }
 }
