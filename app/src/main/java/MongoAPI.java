a47123ad865f66b45146b5d4a7d3db87b8c4c4e6
Paweł Piwkowski <pawel.piwkowski@o2.pl>
2020-01-07 22:43:30 +0100

    Update

---
+++ app/src/main/java/MongoAPI.java
@@ -0,0 +1,7 @@
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+
+public class MongoAPI {
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import java.util.Locale;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private static final    long    startTimeInMilis=3000;
