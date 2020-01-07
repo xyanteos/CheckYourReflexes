cd8982418f5a60527d9cdc92e19e8ddf5ca94d53
Paweł Piwkowski <pawel.piwkowski@o2.pl>
2020-01-07 22:13:57 +0100

    Update
    Update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,28 +7,29 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
     private static final    long    startTimeInMilis=3000;
     private TextView countDownTextView;
     private Button buttonStartPause, buttonSendOnServer, buttonReset;
-
     private CountDownTimer cdt;
     private boolean timeRunning;
     private long timeLeftInMilis = startTimeInMilis;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         countDownTextView = findViewById(R.id.timer);
         buttonStartPause = findViewById(R.id.buttonStart_Pause);
         buttonSendOnServer = findViewById(R.id.buttonWyslanieNaServer);
         buttonReset = findViewById(R.id.reset);
+
+
+
 
         buttonReset.setOnClickListener(new View.OnClickListener() {
             @Override
